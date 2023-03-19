56afcfbc1c478de43ddc82f6d3f4bb418476cd1d
Palak Goel <69416935+palak693@users.noreply.github.com>
2023-03-19 10:06:46 +0530

    Create Go_UsingImport.go

---
+++ Go_UsingImport.go
@@ -0,0 +1,9 @@
+package main
+
+import "math"
+import "fmt"
+
+func main() {
+  fmt.Printf("A person has 25 apples.")
+	fmt.Printf("After obtaining its square roots, he has %g apples.\n", math.Sqrt(25))
+}
