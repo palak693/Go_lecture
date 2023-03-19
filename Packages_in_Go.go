6d09b22c0d8e4c375d8e691b3943f0e6a2b4c453
Palak Goel <69416935+palak693@users.noreply.github.com>
2023-03-19 09:49:37 +0530

    Create Packages_in_Go.go

---
+++ Packages_in_Go.go
@@ -0,0 +1,12 @@
+package main
+import (
+  "fmt"
+  "math/rand")
+
+
+func main(){
+  fmt.Println("Any random number can be given as ", rand.Intn(8))
+  palak := 18
+  fmt.Println("My day of birth is on this number of the month: ", palak)
+
+}
