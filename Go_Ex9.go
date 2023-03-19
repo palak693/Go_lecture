3e90679d520080fa9ebad7d101cf45ba650f3967
Palak Goel <69416935+palak693@users.noreply.github.com>
2023-03-19 11:13:04 +0530

    Create Go_Ex9.go

---
+++ Go_Ex9.go
@@ -0,0 +1,10 @@
+package main //variables with initializers
+
+import "fmt"
+
+var i, j int = 17, 18
+
+func main() {
+	var d, n1, n2 = "Date", "One day prior", "The day after"
+	fmt.Println(i, j, d, n1, n2)
+}
