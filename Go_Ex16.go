bf3f8a8df29e6263fbcf9580ca1c2fbe44d84330
Palak Goel <69416935+palak693@users.noreply.github.com>
2023-03-30 15:17:18 +0530

    Delete Go_Ex16.go

--- Go_Ex16.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func needInt(x int)int{return x*20 + 2}
-func needFloat(x float64)float64{return x*0.2}
-
-func main() {
-  fmt.Println(needInt(4))
-fmt.Println(needInt(3))
-}
